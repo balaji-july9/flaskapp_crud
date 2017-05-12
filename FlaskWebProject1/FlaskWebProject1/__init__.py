4404c3f092d3ab1fc1f7fe070767ac88fed493af
Gaurav Chauhan <gaurav.chauhan36@gmail.com>
2017-05-12 15:05:25 +0530

    Working part 1

--- FlaskWebProject1/FlaskWebProject1/__init__.py
+++ FlaskWebProject1/FlaskWebProject1/__init__.py
@@ -3,6 +3,10 @@ The flask application package.
 """
 
 from flask import Flask
+
+
 app = Flask(__name__)
 
 import FlaskWebProject1.views
+
+
--- FlaskWebProject1/FlaskWebProject1/views.py
+++ FlaskWebProject1/FlaskWebProject1/views.py
@@ -3,35 +3,83 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, json, request
 from FlaskWebProject1 import app
+from werkzeug import generate_password_hash, check_password_hash
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'bucketlist'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
-    return render_template(
-        'index.html',
+    return render_template('index.html',
         title='Home Page',
-        year=datetime.now().year,
-    )
+        year=datetime.now().year,)
 
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
-    return render_template(
-        'contact.html',
+    return render_template('contact.html',
         title='Contact',
         year=datetime.now().year,
-        message='Your contact page.'
-    )
+        message='Your contact page.')
 
 @app.route('/about')
 def about():
     """Renders the about page."""
-    return render_template(
-        'about.html',
+    return render_template('about.html',
         title='About',
         year=datetime.now().year,
-        message='Your application description page.'
-    )
+        message='Your application description page.')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+
+@app.route('/signUp',methods=['POST'])
+def signUp():
+    try:
+        # read the posted values from the UI
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
+ 
+        # validate the received values
+        if _name and _email and _password:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close() 
+        conn.close()
+
+
+
+
+
+
+
+ 
