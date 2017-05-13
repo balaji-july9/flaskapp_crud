6e770e6d1a278bb5ec87f312fdd13e68ac21b355
Gaurav Chauhan <gaurav.chauhan36@gmail.com>
2017-05-13 15:57:07 +0530

    no data in cursor.fetchall

--- FlaskWebProject1/FlaskWebProject1/__init__.py
+++ FlaskWebProject1/FlaskWebProject1/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-
+app.secret_key = 'why would I tell you my secret key?'
 import FlaskWebProject1.views
 
 
--- FlaskWebProject1/FlaskWebProject1/views.py
+++ FlaskWebProject1/FlaskWebProject1/views.py
@@ -1,12 +1,12 @@
 """
 Routes and views for the flask application.
 """
-
-from datetime import datetime
-from flask import render_template, json, request
 from FlaskWebProject1 import app
-from werkzeug import generate_password_hash, check_password_hash
+from datetime import datetime
+from flask import render_template, json, request,redirect,session
 from flaskext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+
 
 mysql = MySQL()
  
@@ -80,10 +80,46 @@ def signUp():
 def showSignin():
     return render_template('signin.html')
 
-
-
-
-
-
-
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        #_username = request.form['inputEmail']
+        #_password = request.form['inputPassword']
+        _username = "gaurav"
+        _password = "gaurav"
  
+ 
+        # connect to mysql
+ 
+        con = mysql.connect()
+        cursor = con.cursor()
+        #sql=cursor.execute("SELECT * FROM tbl_user WHERE user_username = '%s'" % _username)
+        #cursor.execute('sp_validateLogin',(_username,))
+        cursor.callproc('sp_validateLogin',(_username,)) 
+        data = cursor.fetchone()
+ 
+ 
+ 
+ 
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+ 
+ 
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
+        
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
